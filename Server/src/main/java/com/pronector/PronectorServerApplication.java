34842fdc2c9321e0b6f3904c3af7e7e524363464
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2015-10-25 16:06:22 +0100

    File structure modification
    The folder Server contains the server Java project. The front-end folder is the
    folder that contains the fron-end pages.

---
+++ Server/src/main/java/com/pronector/PronectorServerApplication.java
@@ -0,0 +1,12 @@
+package com.pronector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PronectorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PronectorServerApplication.class, args);
+    }
+}
